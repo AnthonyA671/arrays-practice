2f7180cc87b172e051ffb6781de415fc01c1ccb2
TonyL <tony671653@gmail.com>
2023-06-30 17:08:27 -0500

    Nested Arrays practice

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let days = ["Tuesday", "Wednesday", "Thursday"];
-console.log(days.length);
-days.push("Friday");
-days.unshift("Monday");
-let allWeekDays = days.length;
-console.log(allWeekDays);
+// let days = ["Tuesday", "Wednesday", "Thursday"];
+// console.log(days.length);
+// days.push("Friday");
+// days.unshift("Monday");
+// let allWeekDays = days.length;
+// console.log(allWeekDays);
+
+const gameBoard = [
+    ['O', 'X', 'O'],
+    ['O', 'O', 'X'],
+    ['X', 'X', 'O']
+]
+
+console.log(gameBoard);
